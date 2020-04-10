eda2421691a8cb0a10c2af1e7d6e5ca006db9542
Martin Pinter <martin.pinter@vacuumlabs.com>
2020-04-10 18:58:42 +0200

    Remove trama stuff

--- utils/frontendHelper.ts
+++ utils/frontendHelper.ts
@@ -1,9 +1,5 @@
 import download from 'downloadjs'
-import Flexsearch from 'flexsearch'
-import produce from 'immer'
-import _ from 'lodash'
 import qs from 'qs'
-import categoriesIndex from '../public/flexsearchCategoriesIndex.json'
 
 // there are limits to next.js code-splitting, and it can't handle
 // requiring some files on both api routes and frontend
@@ -33,81 +29,4 @@ export const fetchFile = (url, headers, query) => {
     .then(function(blob) {
       download(blob, filename)
     })
-}
-
-// categories helpers
-
-export type Subcategory = {name: string; code: string}
-
-export type Category = {name: string; number: number; categories: Subcategory[]}
-
-export type CategoriesMapped = {
-  [key: string]: {
-    name: string
-    number: number
-    categories: {
-      [key: string]: Subcategory
-    }
-  }
-}
-
-const useMappedCategoriesRecipe = (categories: Category[]): CategoriesMapped => {
-  const categoriesMappedWithArraySubcategories = _.keyBy(categories, 'name')
-  return _.mapValues(categoriesMappedWithArraySubcategories, (category) => ({
-    ...category,
-    categories: _.keyBy(category.categories, 'name'),
-  }))
-}
-
-// we don't want to recalculate this for same value accross different components
-export const useMappedCategories = _.memoize(useMappedCategoriesRecipe)
-
-// the reverse of 'useMappedCategories' hook in CategoriesSection
-export const categoriesToArrays = (categoriesMapped: CategoriesMapped) => {
-  const categoriesMappedWithArraySubcategories = _.mapValues(categoriesMapped, (category) => ({
-    ...category,
-    categories: Object.values(category.categories),
-  }))
-  return Object.values(categoriesMappedWithArraySubcategories)
-}
-
-// if it turns out index is too large to store with codebase, it can be fetched instead of being imported
-const index = Flexsearch.create()
-index.import(JSON.stringify(categoriesIndex))
-
-export const filterFromSearch = async (query: string, categories: Category[]) => {
-  // we want the categories and subcategories sorted by original order instead of relevance (?)
-  // this is according to designs, it also makes our lives a bit easier here
-  const foundIndexes = (await index.search(query)) as number[]
-  const sortedUniqueIndexes = _.uniq(foundIndexes.sort((a, b) => a - b))
-  return sortedUniqueIndexes.reduce((result, id) => {
-    // index stores each searchable record by id
-    // this is close to using 'codes' of subcategories, which are not usable for indexing as they contain duplicates
-    // for more details check the comments on top of 'categories.ts'
-    const categoryIndex = Math.floor(id / 10000) // we encode categories as "their_index_in_category_array * 10000"
-    const subcategoryIndex = (id % 10000) - 1 // we encode subcategories as "parent_category_index + index_in_subcategories_array + 1"
-    if (id % 10000) {
-      // a subcategory is matched, add if not already present
-      if (result[result.length - 1]?.name !== categories[categoryIndex].name) {
-        const categoryWithSingleSubcategory = {
-          ...categories[categoryIndex],
-          categories: [categories[categoryIndex].categories[subcategoryIndex]],
-        }
-        return result.concat(categoryWithSingleSubcategory)
-      } else {
-        // since we are working on sorted array, the parent category is always last in array if it exists
-        return produce(result, (draft) => {
-          draft[draft.length - 1].categories.push(
-            categories[categoryIndex].categories[subcategoryIndex],
-          )
-        })
-      }
-    } else {
-      // whole category is matched - add it, but without subcategories
-      return result.concat({
-        ...categories[categoryIndex],
-        categories: [],
-      })
-    }
-  }, [])
 }
