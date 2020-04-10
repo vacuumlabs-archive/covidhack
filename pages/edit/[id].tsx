1ec2deb47dc21e6bb5bce5dd98d75cca0085aaf3
Martin Pinter <martin.pinter@vacuumlabs.com>
2020-04-10 19:02:02 +0200

    Remove forgotten piece of trama

--- pages/edit/[id].tsx
+++ pages/edit/[id].tsx
@@ -1,41 +1,14 @@
 import {GetServerSideProps} from 'next'
 import React from 'react'
 import Layout from '../../components/Layout'
-import SuccessInfo from '../../components/SuccessInfo'
 import {client} from '../../utils/gql'
-import {
-  RegistrationRequestSuccessQueryQuery,
-  VerificationRequestCategoryQueryQuery,
-} from '../../utils/graphqlSdk'
 
-const SuccessRegistration = (
-  props: RegistrationRequestSuccessQueryQuery['registration_request_by_pk'] &
-    VerificationRequestCategoryQueryQuery['verification_request_by_pk'],
-) => {
+const SuccessRegistration = () => {
   return (
     <>
       <Layout isFormPage>
         <div className="container">
-          <div className="wrapper">
-            <img src="/images/plant.svg" alt="plant" className="img" />
-            <SuccessInfo
-              title={`${
-                props.category === 'international'
-                  ? 'Vaša žiadosť bola odoslaná!'
-                  : 'Vaša objednávka bola odoslaná!'
-              }`}
-              subtitle={`${
-                props.category === 'international'
-                  ? 'Najneskôr nasledujúci pracovný deň Vás s informáciami o ďalšom postupe kontaktuje náš právnik.'
-                  : `Na adresu <span><i>${props.contact_email}</i></span> sme Vám odoslali informácie o ďalšom postupe. Prosím, nasledujte ďalšie kroky z emailu.`
-              }`}
-              note={`${props.category === 'international' ? '' : 'Nedostali ste email?'}`}
-              href=""
-              link={`${
-                props.category === 'international' ? '' : 'Kliknite sem pre odoslanie znova.'
-              }`}
-            />
-          </div>
+          <div className="wrapper">Hura!</div>
         </div>
       </Layout>
       <style jsx>{`
