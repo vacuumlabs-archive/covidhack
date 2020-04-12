901600279cbbb3d35cacf4d99bce5881191878bf
Klara Fickova <klara.fickova@kiwi.com>
2020-04-12 15:53:58 +0200

    Add row and column labels with highlighting

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -23,6 +23,19 @@ export const mapLabResultsToGrid = (labResults: Lab_Result[]) => {
   return grid
 }
 
+const rowLabels = Array.from('ABCDEFGH').map((value) => ({
+  value,
+  readOnly: true,
+}))
+const colLabels = ['', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((value) => ({
+  value: String(value),
+  readOnly: true,
+}))
+
+export const addFrame = (grid) =>
+  [colLabels].concat(grid.map((row, i) => [rowLabels[i]].concat(row)))
+export const removeFrame = (grid) => grid.slice(1).map((row) => row.slice(1))
+
 // https://stackoverflow.com/a/10834843
 export const isNormalInteger = (str) => {
   return /^\+?(0|[1-9]\d*)$/.test(str)
