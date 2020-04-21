ec52452c298c1c296a6fe8923c0ca78073a363a5
Emanuel Tesar <e.tesarr@gmail.com>
2020-04-21 11:20:41 +0200

    Small UI improvements

--- components/lab/CellLegend.tsx
+++ components/lab/CellLegend.tsx
@@ -3,17 +3,6 @@ import {makeStyles} from '@material-ui/styles'
 import classnames from 'classnames'
 import React from 'react'
 
-const useStyles = makeStyles({
-  legendEntry: {
-    textAlign: 'center',
-    verticalAlign: 'middle',
-    color: 'rgba(0, 0, 0, 0.87) !important',
-    border: '1px solid gray !important',
-    borderLeft: 'unset !important',
-    borderRadius: '0 !important',
-  },
-})
-
 export type CellType =
   | 'normal'
   | 'broken'
@@ -23,11 +12,63 @@ export type CellType =
 
 export const LAB_TABLE_BACKGROUNDS: Record<CellType, string> = {
   normal: 'unset',
-  broken: 'yellow',
-  positiveControl: 'lightgreen',
-  negativeControl: 'lightcoral',
-  internalControl: 'lightblue',
+  broken: '#ffff00',
+  positiveControl: '#90ee90',
+  negativeControl: '#f08080',
+  internalControl: '#add8e6',
 }
+
+const useStyles = makeStyles({
+  legendEntry: {
+    textAlign: 'center',
+    verticalAlign: 'middle',
+    color: 'rgba(0, 0, 0, 0.87) !important',
+    border: '1px solid gray !important',
+    borderLeft: 'unset !important',
+    borderRadius: '0 !important',
+  },
+  legendNormal: {
+    borderLeft: '1px solid gray !important',
+    backgroundColor: `${LAB_TABLE_BACKGROUNDS['normal']} !important`,
+  },
+  legendBroken: {
+    backgroundColor: `${LAB_TABLE_BACKGROUNDS['broken']} !important`,
+  },
+  legendPositive: {
+    backgroundColor: `${LAB_TABLE_BACKGROUNDS['positiveControl']} !important`,
+  },
+  legendNegative: {
+    backgroundColor: `${LAB_TABLE_BACKGROUNDS['negativeControl']} !important`,
+  },
+  legendInternalControl: {
+    backgroundColor: `${LAB_TABLE_BACKGROUNDS['internalControl']} !important`,
+  },
+  legendButtonNormal: {
+    '&:hover': {
+      backgroundColor: '#d5d5d5', // default mui hover color
+    },
+  },
+  legendButtonBroken: {
+    '&:hover': {
+      backgroundColor: '#e6e600',
+    },
+  },
+  legendButtonPositive: {
+    '&:hover': {
+      backgroundColor: '#82d682',
+    },
+  },
+  legendButtonNegative: {
+    '&:hover': {
+      backgroundColor: '#d87373',
+    },
+  },
+  legendButtonInternalControl: {
+    '&:hover': {
+      backgroundColor: '#9cc2cf',
+    },
+  },
+})
 
 export interface Props {
   onSetSelectedCellsStatus: (cellType: CellType) => void
@@ -43,12 +84,12 @@ const CellLegend = ({onSetSelectedCellsStatus, selectable}: Props) => {
         size="small"
         variant="contained"
         onClick={() => onSetSelectedCellsStatus('normal')}
-        className={classnames(classes.legendEntry)}
+        className={classnames(
+          classes.legendEntry,
+          classes.legendNormal,
+          selectable && classes.legendButtonNormal,
+        )}
         disabled={!selectable}
-        style={{
-          borderLeft: '1px solid gray !important',
-          backgroundColor: LAB_TABLE_BACKGROUNDS['normal'],
-        }}
       >
         Normálne políčko
       </Button>
@@ -56,9 +97,12 @@ const CellLegend = ({onSetSelectedCellsStatus, selectable}: Props) => {
         size="small"
         variant="contained"
         onClick={() => onSetSelectedCellsStatus('positiveControl')}
-        className={classnames(classes.legendEntry)}
+        className={classnames(
+          classes.legendEntry,
+          classes.legendPositive,
+          selectable && classes.legendButtonPositive,
+        )}
         disabled={!selectable}
-        style={{backgroundColor: LAB_TABLE_BACKGROUNDS['positiveControl']}}
       >
         Pozitívna kontrola
       </Button>
@@ -66,9 +110,12 @@ const CellLegend = ({onSetSelectedCellsStatus, selectable}: Props) => {
         size="small"
         variant="contained"
         onClick={() => onSetSelectedCellsStatus('negativeControl')}
-        className={classnames(classes.legendEntry)}
+        className={classnames(
+          classes.legendEntry,
+          classes.legendNegative,
+          selectable && classes.legendButtonNegative,
+        )}
         disabled={!selectable}
-        style={{backgroundColor: LAB_TABLE_BACKGROUNDS['negativeControl']}}
       >
         Negatívna kontrola
       </Button>
@@ -76,9 +123,12 @@ const CellLegend = ({onSetSelectedCellsStatus, selectable}: Props) => {
         size="small"
         variant="contained"
         onClick={() => onSetSelectedCellsStatus('internalControl')}
-        className={classnames(classes.legendEntry)}
+        className={classnames(
+          classes.legendEntry,
+          classes.legendInternalControl,
+          selectable && classes.legendButtonInternalControl,
+        )}
         disabled={!selectable}
-        style={{backgroundColor: LAB_TABLE_BACKGROUNDS['internalControl']}}
       >
         Interná kontrola
       </Button>
@@ -86,9 +136,12 @@ const CellLegend = ({onSetSelectedCellsStatus, selectable}: Props) => {
         size="small"
         variant="contained"
         onClick={() => onSetSelectedCellsStatus('broken')}
-        className={classnames(classes.legendEntry)}
+        className={classnames(
+          classes.legendEntry,
+          classes.legendBroken,
+          selectable && classes.legendButtonBroken,
+        )}
         disabled={!selectable}
-        style={{backgroundColor: LAB_TABLE_BACKGROUNDS['broken']}}
       >
         Nefunkčné políčko
       </Button>
